a3e4a21926c82582064dcb4ccb8de4fdd66a3633
tonyawalker311 <tonyawalker311@gmail.com>
2025-08-09 16:21:51 -0500

    Delete aireadiness directory

--- aireadiness/vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// GitHub Pages base for repo: aireadiness (username: tonyawalker311)
-export default defineConfig({
-  plugins: [react()],
-  base: '/aireadiness/'
-})
